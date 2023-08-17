3521cd0650577ebdfbeb976b440f97b4e3836626
Rafael Lima <rafael.lima@pinchfinancial.com>
2023-08-16 20:09:14 -0400

    initial commit

---
+++ src/main/java/com/pureplate/PureplateApplication.java
@@ -0,0 +1,13 @@
+package com.pureplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PureplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PureplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pureplate/PureplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pureplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PureplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
