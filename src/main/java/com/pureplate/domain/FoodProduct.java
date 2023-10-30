bc8fe2b5a55d9c0cb8b385d74761270f5291a2f9
ralima <rlprafa@gmail.com>
2023-10-30 19:57:45 -0400

    partial template commit

--- src/main/java/com/pureplate/domain/FoodProduct.java
+++ src/main/java/com/pureplate/domain/FoodProduct.java
@@ -3,6 +3,7 @@ package com.pureplate.domain;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.ToString;
 
@@ -12,6 +13,8 @@ import lombok.ToString;
 @ToString
 public class FoodProduct extends GenericEntity {
 
+  @NotBlank(message = "Name is mandatory")
   private String name;
+  @NotBlank(message = "Description is mandatory")
   private String description;
 }
