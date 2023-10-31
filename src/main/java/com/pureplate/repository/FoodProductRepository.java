459d06f4ac8ee702f22b6e6da27d72abe0fd0774
ralima <rlprafa@gmail.com>
2023-10-30 21:08:36 -0400

    fixing typo

--- src/main/java/com/pureplate/repository/FoodProductRepository.java
+++ src/main/java/com/pureplate/repository/FoodProductRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 import java.util.UUID;
 
 public interface FoodProductRepository  extends CrudRepository<FoodProduct, UUID> {
-  List<FoodProduct> findByName(String title);
+  List<FoodProduct> findByName(String name);
 }
